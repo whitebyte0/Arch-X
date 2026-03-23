3d5f89e47c0e2d347c24cd0ab6a4ab7e45a33b95
whitebyte <romankartshikyan@vektor-x.com>
2026-03-24 02:13:10 +0400

    neovim collapse/expand features enabled

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -5,3 +5,8 @@ o.tabstop = 4
 o.shiftwidth = 4
 o.softtabstop = 4
 o.expandtab = true
+
+-- Treesitter-based code folding
+o.foldmethod = "expr"
+o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+o.foldlevel = 99
